8762fada00ad909c3279a79df9d3fce3fc4d07c7
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-12-19 10:46:20 +0900

    상품디테일 (상품 설명) 내용 추가 및 css 설정 함

--- src/commponents/SingleProduct/SingleProductPage.jsx
+++ src/commponents/SingleProduct/SingleProductPage.jsx
@@ -39,7 +39,20 @@ const SingleProductPage = () => {
         />
       </div>
 
-      <div className="single_product_details"></div>
+      <div className="single_product_details">
+        <h1 className="single_product_title">{product.title}</h1>
+        <p className="single_product_description">{product.description}</p>
+        <p className="single_product_price">
+          ￦ {product.price.toLocaleString("ko-KR")} 원
+        </p>
+
+        <h2 className="quantity_title">구매개수:</h2>
+        <div className="align_center quantity_input">
+          {/* <QuantityInput /> */}
+        </div>
+
+        <button className="search_button add_cart">장바구니 추가</button>
+      </div>
     </section>
   );
 };
