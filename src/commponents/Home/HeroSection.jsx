ca0ac205c32c67f2b8c97f2666551b8d876982dc
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-12-19 09:42:20 +0900

    props로 title, subtitle, link, image를 입력 받도록 함

--- src/commponents/Home/HeroSection.jsx
+++ src/commponents/Home/HeroSection.jsx
@@ -1,22 +1,19 @@
 import "./HeroSection.css";
 import iphone from "../../assets/iphone-14-pro.webp";
 
-const HeroSection = () => {
+const HeroSection = ({ title, subtitle, link, image }) => {
   return (
     <section className="hero_section">
       <div className="align_center">
-        <h2 className="hero_title">아이폰 14 구매하세요</h2>
-        <p className="hero_subtitle">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
-          quibusdam.
-        </p>
-        <a href="#" className="hero_link">
+        <h2 className="hero_title">{title}</h2>
+        <p className="hero_subtitle">{subtitle}</p>
+        <a href={link} className="hero_link">
           바로구매
         </a>
       </div>
 
       <div className="align_center">
-        <img src={iphone} alt="" className="hero_image" />
+        <img src={image} alt="" className="hero_image" />
       </div>
     </section>
   );
