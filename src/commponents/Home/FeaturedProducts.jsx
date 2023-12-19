a89cde35e013b809863e5e41ea541ee967fac6ad
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-12-19 10:27:52 +0900

    productCard 추가 및 productsCard 내부 내용이랑 변경

--- src/commponents/Home/FeaturedProducts.jsx
+++ src/commponents/Home/FeaturedProducts.jsx
@@ -1,4 +1,4 @@
-import ProductCard from "../Products/ProductsCard";
+import ProductCard from "../Products/ProductCard";
 import "./FeaturedProducts.css";
 
 const FeaturedProducts = () => {
--- src/commponents/Products/ProductsCard.jsx
+++ src/commponents/Products/ProductsCard.jsx
@@ -1,36 +1,10 @@
-import "./ProductsCard.css";
-import iphone from "../../assets/iphone.jpg";
-import star from "../../assets/white-star.png";
-import basket from "../../assets/basket.png";
-
-const ProductCard = () => {
+const ProductsPage = () => {
   return (
-    <article className="product_card">
-      <div className="product_image">
-        <a href="product/1">
-          <img src={iphone} alt="product image" />
-        </a>
-      </div>
-
-      <div className="product_details">
-        <h3 className="product_price">120만원</h3>
-        <p className="product_title">iPhone 14 PRO</p>
-
-        <footer className="align_center product_info_footer">
-          <div className="align_center">
-            <p className="align_center product_rating">
-              <img src={star} alt="star" /> 5.0
-            </p>
-            <p className="product_review_count">120</p>
-          </div>
-
-          <button className="add_to_cart">
-            <img src={basket} alt="basket button" />
-          </button>
-        </footer>
-      </div>
-    </article>
+    <section className="products_page">
+      {/* 왼쪽 카테고리 */}
+      {/* 상품목록 */}
+    </section>
   );
 };
 
-export default ProductCard;
+export default ProductsPage;
