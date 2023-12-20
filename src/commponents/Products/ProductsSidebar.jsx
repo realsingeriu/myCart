16700b932ced5b447f642977d80e2a6cc921c937
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-12-20 11:08:49 +0900

    Hook 생성 및 useData를 사용해서 productSidebar 카테고리 변경

--- src/commponents/Products/ProductsSidebar.jsx
+++ src/commponents/Products/ProductsSidebar.jsx
@@ -3,17 +3,10 @@ import rocket from "../../assets/rocket.png";
 import LinkWithIcon from "../Navbar/LinkWithIcon";
 import { useEffect, useState } from "react";
 import apiClient from "../../utils/api-client";
+import useData from "../../Hook/useData";
 
 const ProductsSidebar = () => {
-  const [categories, setCategories] = useState([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get("/category")
-      .then((res) => setCategories(res.data))
-      .catch((err) => setError(err.message));
-  }, []);
+  const { data: categories, error } = useData("category");
 
   return (
     <aside className="products_sidebar">
@@ -24,15 +17,16 @@ const ProductsSidebar = () => {
         {error && <em className="form_error">{error}</em>}
 
         {/* Mapping through categories and rendering LinkWithIcon for each category */}
-        {categories.map((category) => (
-          <LinkWithIcon
-            key={category._id}
-            title={category.name}
-            link={`products?category=${category.name}`}
-            emoji={`http://localhost:5000/category/${category.image}`}
-            sidebar={true}
-          />
-        ))}
+        {categories &&
+          categories.map((category) => (
+            <LinkWithIcon
+              key={category._id}
+              title={category.name}
+              link={`products?category=${category.name}`}
+              emoji={`http://localhost:5000/category/${category.image}`}
+              sidebar={true}
+            />
+          ))}
       </div>
     </aside>
   );
