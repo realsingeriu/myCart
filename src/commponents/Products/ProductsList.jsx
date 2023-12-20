36d50fbe355dd239728f75e513c16c8528a4fd17
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-12-20 13:57:04 +0900

    페이지네이션 컴포넌트 및 productList 페이지 추가

--- src/commponents/Products/ProductsList.jsx
+++ src/commponents/Products/ProductsList.jsx
@@ -9,16 +9,22 @@ import { useSearchParams } from "react-router-dom";
 const ProductsList = () => {
   const [search, setSearch] = useSearchParams();
   const category = search.get("category");
+  const page = search.get("page");
   const { data, error, isLoading } = useData(
     "/products",
     {
       params: {
         category,
+        page,
       },
     },
-    [category]
+    [category, page]
   );
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
+  const handlePageChange = (page) => {
+    const currentParams = Object.fromEntries([...search]);
+    setSearch({ ...currentParams, page: page });
+  };
 
   return (
     <section className="products_list_section">
@@ -48,6 +54,7 @@ const ProductsList = () => {
               stock={product.stock}
             />
           ))}
+        <button onClick={() => handlePageChange(2)}>페이지 2</button>
       </div>
     </section>
   );
