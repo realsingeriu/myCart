d9fd2ad0205fc27998b0708e9b140bb8dd9b4e24
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-12-20 10:43:31 +0900

    카테고리 데이터 가져오기 및 mongodb 카테고리 name 한글로 변경

--- src/commponents/Products/ProductsSidebar.jsx
+++ src/commponents/Products/ProductsSidebar.jsx
@@ -1,19 +1,38 @@
 import "./ProductsSidebar.css";
 import rocket from "../../assets/rocket.png";
 import LinkWithIcon from "../Navbar/LinkWithIcon";
+import { useEffect, useState } from "react";
+import apiClient from "../../utils/api-client";
 
 const ProductsSidebar = () => {
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    apiClient
+      .get("/category")
+      .then((res) => setCategories(res.data))
+      .catch((err) => setError(err.message));
+  }, []);
+
   return (
     <aside className="products_sidebar">
       <h2>카테고리</h2>
 
       <div className="category_links">
-        <LinkWithIcon
-          title="전자제품"
-          link="products?category=electronics"
-          emoji={rocket}
-          sidebar={true}
-        />
+        {/* Displaying error message if there is an error */}
+        {error && <em className="form_error">{error}</em>}
+
+        {/* Mapping through categories and rendering LinkWithIcon for each category */}
+        {categories.map((category) => (
+          <LinkWithIcon
+            key={category._id}
+            title={category.name}
+            link={`products?category=${category.name}`}
+            emoji={`http://localhost:5000/category/${category.image}`}
+            sidebar={true}
+          />
+        ))}
       </div>
     </aside>
   );
