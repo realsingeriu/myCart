2e69aedd416f30a0f1c1993340a12e38a5774372
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-12-21 10:25:01 +0900

    app에서 토큰정보로 유저관리 및 네브바 메뉴를 로그인 상태에 따라 다르게보여주기

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import LoginPage from "./commponents/Authentication/LoginPage";
 import SignupPage from "./commponents/Authentication/SignupPage";
@@ -7,11 +8,32 @@ import Navbar from "./commponents/Navbar/Navbar";
 import ProductsPage from "./commponents/Products/ProductsPage";
 import Routing from "./commponents/Routing/Routing";
 import SingleProductPage from "./commponents/SingleProduct/SingleProductPage";
+import { jwtDecode } from "jwt-decode";
 
 const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    try {
+      const jwt = localStorage.getItem("token");
+      if (jwt) {
+        const jwtUser = jwtDecode(jwt);
+
+        // 토큰 유효가능 시간 설정 유효기간 지난 토큰의 정보는 삭제
+        if (Date.now() >= jwtUser.exp * 1000) {
+          localStorage.removeItem("token");
+          location.reload();
+        } else {
+          setUser(jwtUser);
+        }
+      }
+    } catch (error) {}
+  }, []);
+
   return (
     <div className="app">
-      <Navbar />
+      {/* 유저정보를 app에서 navbar로 전달 */}
+      <Navbar user={user} />
       <main>
         <Routing />
       </main>
